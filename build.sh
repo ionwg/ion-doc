c142a4c60f81e08b773010bf1efddd636f9d95e9
Les Hazlewood <les@hazlewood.com>
2015-05-31 15:19:29 -0700

    #12: fixed erroneous asciidoc that was causing the build to fail

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -e # exit with nonzero exit code if anything fails
-set -x
-set -v
+#set -x
+#set -v
 
 #asciidoctor_installed=$(gem list -i asciidoctor)
 #pygments_installed=$(gem list -i pygments)
@@ -19,11 +19,9 @@ set -v
 #    printf '%s\n' "Installed pygments"
 #fi
 
-printf '%s\n' "Running asciidoctor"
 asciidoctor --safe -D build draft-ion.adoc
-printf '%s\n' "Ran asciidoctor"
 
-set +v
-set +x
+#set +v
+#set +x
 
 #rake foo
